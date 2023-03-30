c26f71a8d95828096ba6350a551510ca1e9119ad
Joe Zakielarz <jjzfive@gmail.com>
2023-03-30 19:23:21 -0400

    saving settings and firestore

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 - Set up firestore database in test mode
+
+- Firestore database rules -> set: allow read, write: if request.auth != null;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,11 @@
-import Head from "next/head";
 import Image from "next/image";
 import { useState, useEffect } from "react";
 import { initializeApp } from "firebase/app";
 import firebaseConfig from "../firebase/config";
 const app = initializeApp(firebaseConfig);
 import { getStorage, ref, uploadBytes, getDownloadURL } from "firebase/storage";
-
-const storage = getStorage(app);
+import toast, { Toaster } from "react-hot-toast";
+import { SyncLoader } from "react-spinners";
 
 import {
   getAuth,
@@ -14,25 +13,22 @@ import {
   GoogleAuthProvider,
   signOut,
 } from "firebase/auth";
-import {
-  getFirestore,
-  collection,
-  getDocs,
-  addDoc,
-  Timestamp,
-  query,
-  orderBy,
-} from "firebase/firestore";
+import { getFirestore, setDoc, doc, getDoc } from "firebase/firestore";
 import { useAuthState } from "react-firebase-hooks/auth";
-import { useCollectionData } from "react-firebase-hooks/firestore";
 
 const auth = getAuth(app);
 const firestore = getFirestore(app);
+const storage = getStorage(app);
 
 export default function Home() {
+  const [loadingUserdata, setLoadingUserdata] = useState<boolean>(true);
   const [refresh, setRefresh] = useState<boolean>(false);
   const [user, loading, error] = useAuthState(auth);
   const [name, setName] = useState("");
+  const [biography, setBiography] = useState("");
+  const [year, setYear] = useState("");
+  const [email, setEmail] = useState("");
+  const [foodpoints, setFoodpoints] = useState(0);
   const [image, setImage] = useState<string | null>(null);
   const [orderHistory, setOrderHistory] = useState([
     {
@@ -43,16 +39,51 @@ export default function Home() {
     },
   ]);
 
+  // Get profile photo
   useEffect(() => {
     if (!user) return;
+
+    setLoadingUserdata(true);
+
+    async function getSettings() {
+      const docRef = doc(firestore, "users", user.uid);
+      const docSnap = await getDoc(docRef);
+
+      if (docSnap.exists()) {
+        const data = docSnap.data();
+        setName(data?.name);
+        setBiography(data?.biography);
+        setYear(data?.year);
+        setEmail(user?.email ? user.email : "");
+        console.log(data);
+        setFoodpoints(data?.foodpoints);
+      } else {
+        // If data not pushed yet, push to database!
+
+        await setDoc(
+          doc(firestore, "users", user.uid),
+          {
+            name: user?.displayName ? user?.displayName : "",
+            biography: "",
+            year: "",
+            foodpoints: 999,
+          },
+          { merge: true }
+        );
+
+        setName(user?.displayName ? user?.displayName : "");
+        setBiography("");
+        setYear("");
+      }
+    }
+
     async function getImage() {
-      const imageRef = ref(storage, "images/" + "USERID HERE");
+      const imageRef = ref(storage, "images/" + user.uid);
 
       // Get the download URL
       getDownloadURL(imageRef)
         .then((url) => {
           // Insert url into an <img> tag to "download"
-          console.log("FOUND URL", url);
           setImage(url);
         })
         .catch((error) => {
@@ -82,16 +113,45 @@ export default function Home() {
         });
     }
 
-    getImage();
+    async function loadEverything() {
+      await getSettings();
+      await getImage();
+      setLoadingUserdata(false);
+    }
+
+    loadEverything();
   }, [user, refresh]);
+
+  async function saveSettings() {
+    if (!user) return;
+
+    try {
+      await setDoc(
+        doc(firestore, "users", user.uid),
+        {
+          name: name,
+          biography: biography,
+          year: year,
+        },
+        { merge: true }
+      );
+
+      toast("Saved settings", {
+        icon: "✅",
+      });
+    } catch (error) {
+      toast("Could not save settings", {
+        icon: "❌",
+      });
+    }
+  }
 
   async function uploadImage(img: File | null) {
     if (!img) return;
-    const storageRef = ref(storage, "images/" + "USERID HERE");
+    const storageRef = ref(storage, "images/" + user.uid);
 
     // 'file' comes from the Blob or File API
     uploadBytes(storageRef, img as File).then((snapshot) => {
-      console.log("Uploaded a blob or file!", snapshot);
       setRefresh(!refresh);
     });
   }
@@ -124,7 +184,19 @@ export default function Home() {
   if (loading) {
     return (
       <main className="min-h-screen bg-gray-100 text-gray-800 text-center">
-        <h1 className="pt-20 text-center text-3xl">Loading...</h1>
+        <div className="pt-20 text-center text-3xl">
+          <SyncLoader color="black" />
+        </div>
+      </main>
+    );
+  }
+
+  if (error) {
+    return (
+      <main className="min-h-screen bg-gray-100 text-gray-800 text-center">
+        <h1 className="pt-20 text-center text-3xl">
+          Uh oh.. error with Firebase Auth!
+        </h1>
       </main>
     );
   }
@@ -132,17 +204,25 @@ export default function Home() {
   if (!user) {
     return (
       <main className="min-h-screen bg-gray-100 text-gray-800 text-center">
-        <div className="max-w-5xl mx-auto">
+        <div className="max-w-5xl mx-auto px-4">
           <h1 className="pt-20 text-center text-3xl">
             Ignite 2023 Firebase Tutorial
           </h1>
           <h3 className="pt-4 text-xl text-gray-700">
             Remaking Mobile Order with Firebase
           </h3>
+          <a
+            href="https://github.com/JJZFIVE"
+            rel="noreferrer"
+            target="_blank"
+            className="w-fit mt-3"
+          >
+            <p className=" text-gray-600 text-xs mt-3">By Joe Zakielarz</p>
+          </a>
 
           <div className="my-20 border-t-2 border-gray-300" />
           <button
-            className="bg-blue-400 px-4 py-2 border border-black text-lg rounded-lg hover:opacity-80"
+            className="bg-black px-4 py-2 text-white text-lg rounded-lg hover:opacity-80"
             onClick={signInWithGoogle}
           >
             Sign In With Google
@@ -175,20 +255,57 @@ export default function Home() {
     },
   ];
 
+  // Renders this if fetchnig user data
+  if (user && loadingUserdata) {
+    return (
+      <main className="min-h-screen bg-gray-100 text-gray-800 text-center">
+        <div className="max-w-5xl mx-auto px-4">
+          <h1 className="pt-20 text-center text-3xl">
+            Ignite 2023 Firebase Tutorial
+          </h1>
+          <h3 className="pt-4 text-xl text-gray-700">
+            Remaking Mobile Order with Firebase
+          </h3>
+          <a
+            href="https://github.com/JJZFIVE"
+            rel="noreferrer"
+            target="_blank"
+            className="w-fit mt-3"
+          >
+            <p className=" text-gray-600 text-xs mt-3">By Joe Zakielarz</p>
+          </a>
+
+          <div className="my-20 border-t-2 border-gray-300" />
+          <SyncLoader color="black" />
+        </div>
+      </main>
+    );
+  }
+
   return (
     <main className="min-h-screen bg-gray-100 text-gray-800 text-center ">
-      <div className="max-w-5xl mx-auto">
+      <Toaster />
+      <div className="max-w-5xl mx-auto px-4">
         <h1 className="pt-20 text-center text-3xl">
           Ignite 2023 Firebase Tutorial
         </h1>
         <h3 className="pt-4 text-xl text-gray-700">
           Remaking Mobile Order with Firebase
         </h3>
+        <a
+          href="https://github.com/JJZFIVE"
+          rel="noreferrer"
+          target="_blank"
+          className="w-fit mt-3"
+        >
+          <p className=" text-gray-600 text-xs mt-3">By Joe Zakielarz</p>
+        </a>
 
         {/* Order */}
-        <h2 className="pt-4 mt-8 font-bold text-left border-t-2 border-gray-300">
-          Order
-        </h2>
+        <div className="flex justify-between pt-4 mt-8  border-t-2 border-gray-300 ">
+          <h2 className="font-bold text-left text-xl">Order</h2>
+          <p>Food Points: ${foodpoints}</p>
+        </div>
         <table className="w-full border-collapse border border-gray-200 mt-8">
           <thead>
             <tr className="border border-gray-200">
@@ -221,7 +338,7 @@ export default function Home() {
         </table>
 
         {/* Order History */}
-        <h2 className="pt-4 mt-8 font-bold text-left border-t-2 border-gray-300">
+        <h2 className="pt-4 mt-8 font-bold text-left border-t-2 border-gray-300 text-xl">
           Order History
         </h2>
         <table className="w-full border-collapse border border-gray-200 mt-2">
@@ -250,17 +367,45 @@ export default function Home() {
         </table>
 
         {/* Settings */}
-        <h2 className="pt-4 mt-8 font-bold text-left border-t-2 border-gray-300">
+        <h2 className="pt-4 mt-8 font-bold text-left border-t-2 border-gray-300 text-xl">
           Settings
         </h2>
-        <div className="mt-2 flex  bg-blue-300">
+        <div className="mt-2 flex">
           <div className="flex flex-col gap-3 w-full">
-            <input
-              className="border border-gray-200 rounded-lg px-4 py-2 w-1/2"
-              placeholder="Name"
-              onChange={(e) => setName(e.target.value)}
-              value={name}
-            />
+            <div className="flex w-full items-center">
+              <label className="text-lg w-36 text-left">Name: </label>
+              <input
+                className="border border-gray-200 rounded-lg px-4 py-2 w-1/2"
+                placeholder="Name"
+                onChange={(e) => setName(e.target.value)}
+                value={name}
+              />
+            </div>
+            <div className="flex w-full items-center gap-4">
+              <label className="text-lg w-32 text-left">Biography: </label>
+              <input
+                className="border border-gray-200 rounded-lg px-4 py-2 w-1/2"
+                placeholder="Biography"
+                onChange={(e) => setBiography(e.target.value)}
+                value={biography}
+              />
+            </div>
+            <div className="flex w-full items-center gap-4">
+              <label className="text-lg w-32 text-left">Year: </label>
+              <input
+                className="border border-gray-200 rounded-lg px-4 py-2 w-1/2"
+                placeholder="Year"
+                onChange={(e) => setYear(e.target.value)}
+                value={year}
+              />
+            </div>
+
+            <button
+              className="mx-auto px-10 py-3 border-2 border-black hover:opacity-80 rounded-lg"
+              onClick={saveSettings}
+            >
+              Save
+            </button>
           </div>
           <div className="flex flex-col w-full items-center justify-center">
             {image ? (
@@ -283,16 +428,19 @@ export default function Home() {
                   uploadImage(e.target.files[0]);
                 }
               }}
+              className="ml-28"
             />
           </div>
         </div>
 
-        <button
-          className="bg-black px-12 py-3 rounded-lg text-white hover:opacity-80"
-          onClick={() => signOut(auth)}
-        >
-          Log Out
-        </button>
+        <div className="py-8 mt-4 border-t-2 border-gray-300">
+          <button
+            className="bg-black px-12 py-3 rounded-lg text-white hover:opacity-80"
+            onClick={() => signOut(auth)}
+          >
+            Log Out
+          </button>
+        </div>
       </div>
     </main>
   );
